8ee2fa7561a01c168b9c357dee94456413c45660
Ajay <ajay10mar96@gmail.com>
2017-05-09 19:17:10 +0530

    2

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('username')->nullable();
-            $table->string('email')->unique();
+            $table->string('email', 400)->unique();
             $table->string('password')->nullable();
             $table->string('firstname');
             $table->string('lastname')->nullable();
